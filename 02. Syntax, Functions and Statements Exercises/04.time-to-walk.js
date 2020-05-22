82963cf6c0cb99d3b88146d8a56993eb8c1b3549
IvayloDechkoff <Stradjazz1973@gmail.com>
2020-05-22 10:26:27 +0200

    Corrections

--- 02. Syntax, Functions and Statements Exercises/04.time-to-walk.js
+++ 02. Syntax, Functions and Statements Exercises/04.time-to-walk.js
@@ -1,16 +1,25 @@
 function timeToWalk(steps, sizeOfStep, speed) {
     let distance = (steps * sizeOfStep) / 1000;
     let breaks = Math.floor((distance * 1000) / 500);
-    let timeInSeconds = (breaks * 60) + ((distance / speed) * 3600);
-    let walkingHours = Math.floor(timeInSeconds / 3600);
+    let timeInSeconds = Math.ceil((breaks * 60) + ((distance / speed) * 3600));
+    
+    
+
+    let result = new Date(null, null, null, null, null, timeInSeconds);
+    let walkingHours = result.getHours();
     if (walkingHours < 10) {
         walkingHours = '0' + walkingHours;
     }
-    let remainingSeconds = timeInSeconds - (walkingHours * 3600);
-    let walkingMinutes = Math.floor(remainingSeconds / 60);
-    let walkingSeconds = Math.round(remainingSeconds - (walkingMinutes * 60));
+
+    let walkingMinutes = result.getMinutes();
+    if (walkingMinutes < 10) {
+        walkingMinutes = '0' + walkingMinutes;
+    }
+
+    let walkingSeconds = result.getSeconds();
+    if (walkingSeconds < 10) {
+        walkingSeconds = '0' + walkingSeconds;
+    }
 
     console.log(`${walkingHours}:${walkingMinutes}:${walkingSeconds}`);
 }
-
-timeToWalk(2564, 0.70, 5.5);
