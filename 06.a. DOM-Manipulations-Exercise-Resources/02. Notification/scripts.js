a27b8782e89e01093194f307779223429512ffc5
Stradjazz <ivodechkoff@telefonica.net>
2020-06-21 20:45:28 +0200

    Corrections

--- 06.a. DOM-Manipulations-Exercise-Resources/02. Notification/scripts.js
+++
@@ -1,7 +0,0 @@
-function notify(message) {
-    const notificationDiv = document.getElementById('notification');
-    notificationDiv.textContent = message;
-    notificationDiv.style.display = 'block';
-
-    setTimeout(function(){notificationDiv.style.display = 'none';}, 2000);
-}
